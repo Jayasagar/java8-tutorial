1d68e785b5c4f0150f9ed23d743805cab18eb6d4
Jayasagar <jayasagar@jayasagars-mbp.localdomain>
2016-07-09 22:16:20 +0200

    basic filter

---
+++ src/com/geeklabs/java8/inventory/Inventory.java
@@ -0,0 +1,20 @@
+package com.geeklabs.java8.inventory;
+
+import com.geeklabs.java8.model.Apple;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Inventory {
+    public static void main(String[] args) {
+        List<Apple> apples = new ArrayList<Apple>();
+        apples.add(new Apple("Swis", "red", 123, 150));
+        apples.add(new Apple("Smila", "brown", 150, 120));
+        apples.add(new Apple("Himalaya", "grren", 175, 200));
+
+        Stream<Apple> appleStream = apples.stream().filter(apple -> apple.getWeight() >= 150);
+
+        System.out.print(appleStream.toArray());
+    }
+}
---
+++ src/com/geeklabs/java8/model/Apple.java
@@ -0,0 +1,47 @@
+package com.geeklabs.java8.model;
+
+public class Apple {
+    private String name;
+    private String color;
+    private double price;
+    private int weight;
+
+    public Apple(String name, String color, double price, int weight) {
+        this.name = name;
+        this.color = color;
+        this.price = price;
+        this.weight = weight;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
